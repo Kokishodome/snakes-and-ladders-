3f467abcdb035370888e2d1c90feca59c87847f3
slay <159949398+Kokishodome@users.noreply.github.com>
2026-06-30 16:41:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let die = document.getElementById('die')
+let pinkSteps = 0
+let playerPink = document.getElementById('playerPink')
+let randomNumber = null
+let randomName = null
 
 die.onclick = function () {
     die.style.transform = 'translate(-50%,-50%) scale(0.2) rotate(1000deg)'
     die.style.transition = '1s'
-    setInterval(() => {
-        let randomNumber = Math.floor(Math.random() * 6) + 1
+    const dieInterval = setInterval(() => {
+        randomNumber = Math.floor(Math.random() * 6) + 1
         die.src = 'dice' + randomNumber + '.png'
     }, 167);
+    setTimeout(() => {
+        clearInterval(dieInterval)
+        playerPink.style.transition = '0.5s'
+        let dieNumber = randomNumber
+        const pawnInterval = setInterval(() => {
+            dieNumber--
+            pinkSteps++
+            playerPink.style.left = pinkSteps + '0%'
+            if (dieNumber == 0) {
+                clearInterval(pawnInterval)
+            }
+        }, 500);
+        // randomName = Math.floor(Math.random())
+    }, 2000);
+
 }
 
-// github and add a second pawn
+// make sure the name 'beauty queen' is random each time with another name
