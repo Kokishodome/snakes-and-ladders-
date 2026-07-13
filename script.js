e95883de45b4bf0083fb16c5101663d61cfec9fe
slay <159949398+Kokishodome@users.noreply.github.com>
2026-07-13 17:54:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ let playerBlue = document.getElementById('playerBlue')
 let randomNumber = null
 let randomName = null
 let beautyQueen = document.getElementById('beautyQueen')
-let isBeautyQueen = true
+let isBeautyQueen = false
+const bonuses = {
+    6: 27, 9: 50, 20: 39, 25: 57, 53: 72, 54: 85, 61: 82,
+    43: 16, 55: 34, 70: 48, 78: 42, 95: 73, 96: 82
+}
 
 function movePawn(steps, player) {
+    let dieNumber = randomNumber
+    if (isBeautyQueen) {
+        pinkSteps = pinkSteps + dieNumber
+    }
+
+    else {
+        blueSteps = blueSteps + dieNumber
+    }
     const pinkInterval = setInterval(() => {
         dieNumber--
         steps++
@@ -31,6 +43,28 @@ function movePawn(steps, player) {
         player.style.bottom = Math.floor(steps / 10) + '0%'
         if (dieNumber == 0) {
             clearInterval(pinkInterval)
+            let cell = steps + 1
+            if (bonuses[cell]) {
+                steps = bonuses[cell] - 1
+
+                if (Math.floor(steps / 10) % 2 == 0) {
+                    player.style.left = steps % 10 + '0%'
+                }
+
+                else {
+                    player.style.left = 9 - steps % 10 + '0%'
+                }
+
+                player.style.bottom = Math.floor(steps / 10) + '0%'
+
+                if (isBeautyQueen) {
+                    pinkSteps = steps
+                }
+
+                else {
+                    blueSteps = steps
+                }
+            }
             die.style.pointerEvents = 'auto'
             die.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
         }
@@ -63,8 +97,7 @@ die.onclick = function () {
         setTimeout(() => {
             clearInterval(dieInterval)
             playerPink.style.transition = '0.5s'
-            let dieNumber = randomNumber
-            movePawn()
+            movePawn(pinkSteps, playerPink)
 
 
         }, 2000);
@@ -75,11 +108,13 @@ die.onclick = function () {
         setTimeout(() => {
             clearInterval(dieInterval)
             playerBlue.style.transition = '0.5s'
-            let dieNumber = randomNumber
-            movePawn()
+            movePawn(blueSteps, playerBlue)
 
         }, 2000);
     }
 
 
 }
+
+
+// read the code
