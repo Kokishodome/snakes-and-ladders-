3e3473575a1c7e13c387c45a7ab05b6dbb09c2b9
slay <159949398+Kokishodome@users.noreply.github.com>
2026-09-19 10:13:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let randomNumber = null
 let randomName = null
 let beautyQueen = document.getElementById('beautyQueen')
 let isBeautyQueen = false
+let clearGame = document.getElementById('clearGame')
 const bonuses = {
     6: 27, 9: 50, 20: 39, 25: 57, 53: 72, 54: 85, 61: 82,
     43: 16, 55: 34, 70: 48, 78: 42, 95: 73, 96: 82
@@ -17,24 +18,40 @@ function movePawn(steps, player) {
     if (isBeautyQueen) {
         pinkSteps = pinkSteps + dieNumber
     }
-
     else {
         blueSteps = blueSteps + dieNumber
     }
+
     const pinkInterval = setInterval(() => {
         dieNumber--
         steps++
 
+        if (steps >= 99) {
+            die.style.transition = '1s'
+            die.style.opacity = '0'
+            beautyQueen.innerHTML = 'Player wins!'
+            clearInterval(pinkInterval)
+            setTimeout(() => {
+                player.style.transition = '0.5s'
+                player.style.transform = 'translateY(-40px)'
+                setTimeout(() => {
+                    player.style.transform = 'translateY(-40px) rotate(360deg)'
+                    setTimeout(() => {
+                        player.style.transform = 'rotate(360deg)'
+                    }, 500);
+                }, 500);
+
+            }, 1000);
+        }
+
         if (steps < 10) {
             player.style.left = steps + '0%'
         }
-
         else {
 
             if (Math.floor(steps / 10) % 2 == 0) {
                 player.style.left = steps % 10 + '0%'
             }
-
             else {
                 player.style.left = 9 - steps % 10 + '0%'
             }
@@ -50,7 +67,6 @@ function movePawn(steps, player) {
                 if (Math.floor(steps / 10) % 2 == 0) {
                     player.style.left = steps % 10 + '0%'
                 }
-
                 else {
                     player.style.left = 9 - steps % 10 + '0%'
                 }
@@ -60,7 +76,6 @@ function movePawn(steps, player) {
                 if (isBeautyQueen) {
                     pinkSteps = steps
                 }
-
                 else {
                     blueSteps = steps
                 }
@@ -72,14 +87,12 @@ function movePawn(steps, player) {
 }
 
 die.onclick = function () {
-
     die.style.pointerEvents = 'none'
 
     if (isBeautyQueen) {
         isBeautyQueen = false
         beautyQueen.innerHTML = 'Player: another one'
     }
-
     else {
         isBeautyQueen = true
         beautyQueen.innerHTML = 'Player: Beauty Queen'
@@ -93,28 +106,33 @@ die.onclick = function () {
     }, 167);
 
     if (isBeautyQueen) {
-
         setTimeout(() => {
             clearInterval(dieInterval)
             playerPink.style.transition = '0.5s'
             movePawn(pinkSteps, playerPink)
-
-
         }, 2000);
-
     }
-
     else {
         setTimeout(() => {
             clearInterval(dieInterval)
             playerBlue.style.transition = '0.5s'
             movePawn(blueSteps, playerBlue)
-
         }, 2000);
     }
-
-
 }
 
+clearGame.onclick = function () {
+    die.style.opacity = '1'
+    die.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+    die.style.pointerEvents = 'auto'
+    playerPink.style.left = '0%'
+    playerPink.style.bottom = '0%'
+    playerBlue.style.left = '-2%'
+    playerBlue.style.bottom = '0%'
+    pinkSteps = 98
+    blueSteps = 0
+    beautyQueen = 'Player: Beauty Queen'
+    isBeautyQueen = false
+}
 
-// read the code
+// get rid of the cube whne the game ends and bring it back when the game restarts >W<
